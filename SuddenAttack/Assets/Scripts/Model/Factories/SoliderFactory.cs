4cdcd776ff26b7db56b66d0a642c7eed6711ee86
englmantin <gonzoglizli@gmail.com>
2019-08-07 13:32:26 +0200

    * unit factory fixes

--- SuddenAttack/Assets/Scripts/Model/Factories/SoliderFactory.cs
+++ SuddenAttack/Assets/Scripts/Model/Factories/SoliderFactory.cs
@@ -6,10 +6,15 @@ public class SoliderFactory : IUnitFactory
 {
     public Unit CreateUnit(float x, float y, GameObject prefab)
     {
+        Vector3 position = new Vector3(x, y, 0);
         var solider = new Solider
         {
             Prefab = Object.Instantiate(prefab)
         };
+
+        solider.Prefab.transform.SetPositionAndRotation(position, solider.Prefab.transform.rotation);
+        solider.Prefab.GetComponent<UnitController>().SetDestination(position);
+
         return solider;
     }
 }
--- SuddenAttack/Assets/Scripts/Model/Factories/TankFactory.cs
+++ SuddenAttack/Assets/Scripts/Model/Factories/TankFactory.cs
@@ -6,10 +6,15 @@ public class TankFactory : IUnitFactory
 {
     public Unit CreateUnit(float x, float y, GameObject prefab)
     {
+        Vector3 position = new Vector3(x, y, 0);
         var tank = new Tank
         {
             Prefab = Object.Instantiate(prefab)
         };
+
+        tank.Prefab.transform.SetPositionAndRotation(position, tank.Prefab.transform.rotation);
+        tank.Prefab.GetComponent<UnitController>().SetDestination(position);
+
         return tank;
     }
 }
